d2f06c69853bb6df336902457f862f63f291571d
Rafael Brito <rafaelbritoh.007@gmail.com>
2023-12-02 12:09:46 -0500

    Se quito la resctriccion para solo reconocer cinco objetos

--- image_geometry_properties.py
+++ image_geometry_properties.py
@@ -36,7 +36,6 @@ class ImageProcessorApp:
         imBn = cv2.bitwise_not(imBn)
         contornos, _ = cv2.findContours(imBn, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contornos = sorted(contornos, key=cv2.contourArea, reverse=True)
-        numObjetosMostrados = min(5, len(contornos))
 
         fig, axs = plt.subplots(2, 2)
 
@@ -44,17 +43,17 @@ class ImageProcessorApp:
         axs[0, 0].set_title('Imagen Original')
 
         axs[0, 1].imshow(imBn, cmap='gray')
-        for i in range(numObjetosMostrados):
-            axs[0, 1].plot(contornos[i][:, 0, 0], contornos[i][:, 0, 1], 'r', linewidth=1)
+        for contorno in contornos:
+            axs[0, 1].plot(contorno[:, 0, 0], contorno[:, 0, 1], 'r', linewidth=1)
         axs[0, 1].set_title('Escala binaria con contornos')
 
         axs[1, 0].imshow(imgGray, cmap='gray')
         axs[1, 0].set_title('Escala de grises')
 
         axs[1, 1].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        for i in range(numObjetosMostrados):
-            axs[1, 1].plot(contornos[i][:, 0, 0], contornos[i][:, 0, 1], 'r', linewidth=1)
-            M = cv2.moments(contornos[i])
+        for contorno in contornos:
+            axs[1, 1].plot(contorno[:, 0, 0], contorno[:, 0, 1], 'r', linewidth=1)
+            M = cv2.moments(contorno)
             if M['m00'] != 0:
                 x = int(M['m10']/M['m00'])
                 y = int(M['m01']/M['m00'])

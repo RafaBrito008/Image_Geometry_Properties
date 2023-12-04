97c790ca4ff6b0327dc251733b4ff3da5647436d
Rafael Brito <rafaelbritoh.007@gmail.com>
2023-12-03 20:52:19 -0500

    Reemplazo del umbral fijo para usar un umbral adaptativo

--- image_geometry_properties_AT.py
+++ image_geometry_properties_AT.py
@@ -28,7 +28,9 @@ class ImageProcessorApp:
 
         # Crear un lienzo para mostrar imágenes
         self.canvas = tk.Canvas(self.root, width=600, height=600)
-        self.canvas.pack(fill=tk.BOTH, expand=True)  # Empaquetar el lienzo en la ventana principal
+        self.canvas.pack(
+            fill=tk.BOTH, expand=True
+        )  # Empaquetar el lienzo en la ventana principal
 
         # Vincular el evento de cierre de la ventana
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
@@ -48,71 +50,84 @@ class ImageProcessorApp:
             self.process_image()  # Si se selecciona una imagen, procesarla
 
     def process_image(self):
-        # Método para procesar la imagen cargada
-        img = cv2.imread(self.image_path)  # Leer la imagen usando OpenCV
-        threshold_value = 170  # Valor de umbral para la binarización
+        # Parámetros configurables para el procesamiento de la imagen
+        # Parámetros configurables para el procesamiento de la imagen
+        max_threshold_value = 255  # Valor máximo para umbralización
+        adaptive_method = cv2.ADAPTIVE_THRESH_GAUSSIAN_C  # Método de umbralización adaptativa
+        threshold_type = cv2.THRESH_BINARY  # Tipo de umbral
+        block_size = 11  # Tamaño del bloque para la umbralización adaptativa
+        subtract_constant = 2  # Constante a restar del cálculo del umbral
+        min_contour_area = 150  # Área mínima para considerar un contorno
 
-        # Convertir la imagen a escala de grises
-        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # Binarizar la imagen
-        _, imBn = cv2.threshold(imgGray, threshold_value, 255, cv2.THRESH_BINARY)
-        # Invertir la imagen binarizada
-        imBn = cv2.bitwise_not(imBn)
-        # Encontrar contornos en la imagen binarizada
-        contornos, _ = cv2.findContours(
-            imBn, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-        )
+        # Cargar la imagen desde la ruta especificada. OpenCV por defecto carga las imágenes en formato BGR.
+        img = cv2.imread(self.image_path)
 
-        # Filtrar contornos que son demasiado pequeños
-        min_contour_area = 150  # Umbral mínimo para el área del contorno
-        # Filtrar contornos por área
-        contornos = [
-            cnt for cnt in contornos if cv2.contourArea(cnt) > min_contour_area
-        ]
+        # Convertir la imagen de BGR a escala de grises para simplificar el análisis y procesamiento.
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Ordenar contornos por área en orden descendente
+        # Aplicar umbralización adaptativa a la imagen en escala de grises.
+        # Esto permite ajustar el umbral para diferentes áreas de la imagen.
+        img_binary = cv2.adaptiveThreshold(img_gray, max_threshold_value, adaptive_method, 
+                                           threshold_type, block_size, subtract_constant)
+
+        # Invertir la imagen binaria para la detección de contornos en OpenCV.
+        img_binary_inv = cv2.bitwise_not(img_binary)
+
+        # Detectar los contornos en la imagen binaria invertida.
+        contornos, _ = cv2.findContours(img_binary_inv, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Filtrar contornos por área mínima.
+        contornos = [cnt for cnt in contornos if cv2.contourArea(cnt) > min_contour_area]
+
+        # Ordenar los contornos por su área, de mayor a menor.
         contornos = sorted(contornos, key=cv2.contourArea, reverse=True)
 
-        # Crear una figura de Matplotlib con 4 subplots
+
+        # Crear una figura con 4 subplots (2x2) para mostrar diferentes transformaciones de la imagen.
         fig, axs = plt.subplots(2, 2)
 
-        # Mostrar la imagen original en el primer subplot
+        # Mostrar la imagen original en el primer subplot.
         axs[0, 0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         axs[0, 0].set_title("Imagen Original")
+        axs[0, 0].axis('off')  # Ocultar los ejes para una mejor visualización.
 
-        # Mostrar la imagen binarizada con contornos en el segundo subplot
-        axs[0, 1].imshow(imBn, cmap="gray")
-        for contorno in contornos:
-            # Dibujar cada contorno
-            axs[0, 1].plot(contorno[:, 0, 0], contorno[:, 0, 1], "r", linewidth=1)
+        # Mostrar la imagen binaria invertida en el segundo subplot y dibujar los contornos encontrados.
+        axs[0, 1].imshow(img_binary_inv, cmap='gray')
         axs[0, 1].set_title("Escala binaria con contornos")
+        axs[0, 1].axis('off')
+        for contour in contornos:
+            # Dibujar el contorno en rojo sobre la imagen binaria invertida.
+            axs[0, 1].plot(contour[:, 0, 0], contour[:, 0, 1], "r", linewidth=1)
 
-        # Mostrar la imagen en escala de grises en el tercer subplot
-        axs[1, 0].imshow(imgGray, cmap="gray")
+        # Mostrar la imagen en escala de grises en el tercer subplot.
+        axs[1, 0].imshow(img_gray, cmap='gray')
         axs[1, 0].set_title("Escala de grises")
+        axs[1, 0].axis('off')
 
-        # Mostrar la imagen original con contornos y centroides en el cuarto subplot
+        # Mostrar la imagen original en el cuarto subplot y dibujar los contornos y centroides.
         axs[1, 1].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        for contorno in contornos:
-            # Dibujar cada contorno
-            axs[1, 1].plot(contorno[:, 0, 0], contorno[:, 0, 1], "r", linewidth=1)
-            # Calcular y dibujar el centroide de cada contorno
-            M = cv2.moments(contorno)
-            if M["m00"] != 0:
-                x = int(M["m10"] / M["m00"])
-                y = int(M["m01"] / M["m00"])
-                axs[1, 1].plot(x, y, "r*", markersize=10)
         axs[1, 1].set_title("Contornos y centroides")
+        axs[1, 1].axis('off')
+        for contour in contornos:
+            # Dibujar el contorno en rojo sobre la imagen original.
+            axs[1, 1].plot(contour[:, 0, 0], contour[:, 0, 1], "r", linewidth=1)
+            # Calcular el centroide del contorno y dibujarlo como una estrella roja.
+            M = cv2.moments(contour)
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                axs[1, 1].plot(cX, cY, "r*", markersize=10)
 
-        # Ajustar etiquetas y mostrar la figura en el lienzo de Tkinter
+        # Ajustar las etiquetas de los ejes y mostrar la figura en el lienzo de Tkinter
         for ax in axs.flat:
             ax.label_outer()
 
-        # Crear un canvas para Matplotlib en Tkinter y mostrar la figura
+        # Integrar la figura de Matplotlib en el lienzo de Tkinter
         canvas = FigureCanvasTkAgg(fig, master=self.canvas)
         canvas_widget = canvas.get_tk_widget()
-        canvas_widget.pack(fill=tk.BOTH, expand=True)  # Empaquetar el widget del canvas
-        canvas.draw()  # Dibujar el canvas
+        canvas_widget.pack(fill=tk.BOTH, expand=True)
+        canvas.draw()
+
 
 
 # Bloque principal para ejecutar la aplicación
